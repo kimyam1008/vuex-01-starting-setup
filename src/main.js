6091f5e0162beca074f4978fd4a78de5d9ba80b7
yoon <kimyam1008@gmail.com>
2025-01-03 10:29:19 +0900

    상태 추가

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@ import App from './App.vue';
 const store = createStore({
     state() {
         return{
-            counter: 0
+            counter: 0,
+            isLoggedIn : false
         };
     },
     mutations: {
@@ -15,6 +16,9 @@ const store = createStore({
         },
         increase(state, payload){
             state.counter = state.counter + payload.value;
+        },
+        setAuth(state, payload){
+            state.isLoggedIn = payload.isAuth;
         }
     },
     actions: {
@@ -26,7 +30,13 @@ const store = createStore({
         increase(context, payload){
             console.log(context);
             context.commit('increase', payload);
-        }
+        },
+        login(context) {
+            context.commit('setAuth', {isAuth: true});
+        },
+        logout(context) {
+            context.commit('setAuth', {isAuth: false});
+        },
     },
     getters: {
         finalCounter(state){
@@ -44,6 +54,9 @@ const store = createStore({
             }
 
             return finalCounter;
+        },
+        userIsAuthenticated(state){
+            return state.isLoggedIn;
         }
     }
 });
